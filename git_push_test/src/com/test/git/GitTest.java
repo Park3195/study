3e395e55ef024e51a14f9a0b7859aa8e6272a53a
big-12 <big-12@BIG-10-PC>
2017-12-07 16:22:37 +0900

    modify GitTest.java : println("와우~~ ....

--- git_push_test/src/com/test/git/GitTest.java
+++ git_push_test/src/com/test/git/GitTest.java
@@ -3,7 +3,7 @@ package com.test.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("이야~~ 내일은 삼겹살 무제한...!!!");
 
 	}
 
