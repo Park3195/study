a05d3f5e19ff78c13f9f44d1082f89c2c49f0d4f
big-12 <big-12@BIG-10-PC>
2017-12-07 16:29:48 +0900

    modify GitTest.java : println("와우!!! ...

--- git_push_test/src/com/test/git/GitTest.java
+++ git_push_test/src/com/test/git/GitTest.java
@@ -3,7 +3,8 @@ package com.test.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("이야~~ 내일은 삼겹살 무제한...!!!");
+
+		System.out.println("와우~~ 내일은 시험보는 날~~!!");
 
 	}
 
